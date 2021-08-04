d9e29787ea2c5daa9d38cf68c894675d8a4fcb2d
Caio Medeiros <63916640+Caio-medeiros@users.noreply.github.com>
2021-08-04 23:07:17 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,24 +1,22 @@
-
-def play_hangman():
     print("\n****************************")
     print("Welcome to the hangman game!")
     print("****************************")
 
     secret_word = "Apple".upper()
-    # para cada letra na palavra adicione no final da lista "_"
+   
     guessed_letters = ["_" for letter in secret_word]
 
     hanged = False
-    got_it_right = False  # O jogo ainda não começou por isso deixamos "False"
+    got_it_right = False
     mistakes = 0
 
-    while(not hanged and not got_it_right):  # Enquanto(True and True) = Enquanto(True)
+    while(not hanged and not got_it_right):
         guess = input("\nWhich Letter? ")
         guess = guess.strip().upper()
 
         if (guess in secret_word):
             index = 0
-            # você não precisa declara explicitamente o tipo da variável que será utilizada no for como índice da execução.
+          
             for letter in secret_word:
                 if(guess == letter):
                     guessed_letters[index] = letter
@@ -37,7 +35,3 @@ def play_hangman():
         print('You won!')
     else:
         print("You lost!")
-
-
-if(__name__ == "__main__"):  # usamos para poder rodar depois separadamente
-    play_hangman()
