bdf525f346f5051bd805be94f3f77492649c5585
Caio Medeiros <63916640+Caio-medeiros@users.noreply.github.com>
2021-08-05 22:16:28 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,13 +1,79 @@
 import random
 
+
+def play_hangman():
+
+    welcome()
+    secret_word = random_secret_word()
+    guessed_letters = right_letters(secret_word)
+
+    hanged = False
+    got_it_right = False 
+    mistakes = 0
+
+    while(not hanged and not got_it_right):
+        guess = input("\nWhich Letter? ")
+        guess = guess.strip().upper()
+
+        if (guess in secret_word):
+            sets_the_right_guess(secret_word, guessed_letters, guess)
+        else:
+            mistakes += 1
+            hangman_art(mistakes)
+            print(f"You made {mistakes} mistakes.")
+
+        if (mistakes == 7):
+            break
+
+        got_it_right = "_" not in guessed_letters
+        print(guessed_letters)
+
+    if (got_it_right):
+        print("Congrats you won!")
+        print("       ___________      ")
+        print("      '._==_==_=_.'     ")
+        print("      .-\\:      /-.    ")
+        print("     | (|:.     |) |    ")
+        print("      '-|:.     |-'     ")
+        print("        \\::.    /      ")
+        print("         '::. .'        ")
+        print("           ) (          ")
+        print("         _.' '._        ")
+        print("        '-------'       ")
+    else:
+        print("Ohh no, you died!")
+        print("The word was {}".format(secret_word))
+        print("    _______________         ")
+        print("   /               \       ")
+        print("  /                 \      ")
+        print("//                   \/\  ")
+        print("\|   XXXX     XXXX   | /   ")
+        print(" |   XXXX     XXXX   |/     ")
+        print(" |   XXX       XXX   |      ")
+        print(" |                   |      ")
+        print(" \__      XXX      __/     ")
+        print("   |\     XXX     /|       ")
+        print("   | |           | |        ")
+        print("   | I I I I I I I |        ")
+        print("   |  I I I I I I  |        ")
+        print("   \_             _/       ")
+        print("     \_         _/         ")
+        print("       \_______/           ")
+
+
+# functions
+
+def welcome():
     print("\n****************************")
     print("Welcome to the hangman game!")
     print("****************************")
 
+
+def random_secret_word():
     file = open("words.txt", "r")
     words = []
 
-    for line in file:
+    for line in file: 
         line = line.strip()
         words.append(line)
 
@@ -15,35 +81,72 @@ import random
 
     number = random.randrange(0, len(words))
     secret_word = words[number].upper()
-    # para cada letra na palavra adicione no final da lista "_"
-    guessed_letters = ["_" for letter in secret_word]
+    return secret_word
 
-    hanged = False
-    got_it_right = False  # O jogo ainda não começou por isso deixamos "False"
-    mistakes = 0
 
-    while(not hanged and not got_it_right):  # Enquanto(True and True) = Enquanto(True)
-        guess = input("\nWhich Letter? ")
-        guess = guess.strip().upper()
+def right_letters(secret_word):
+    return ["_" for letter in secret_word]
 
-        if (guess in secret_word):
-            index = 0
-            # você não precisa declara explicitamente o tipo da variável que será utilizada no for como índice da execução.
-            for letter in secret_word:
-                if(guess == letter):
-                    guessed_letters[index] = letter
-                index += 1
-        else:
-            mistakes += 1
-            print(f"You made {mistakes} mistakes.")
 
-        if (mistakes >= 5):
-            break
+def sets_the_right_guess(secret_word, guessed_letters, guess):
+    index = 0
+    for letter in secret_word:
+        if(guess == letter):
+            guessed_letters[index] = letter
+        index += 1
 
-        got_it_right = "_" not in guessed_letters
-        print(guessed_letters)
 
-    if (got_it_right):
-        print('You won!')
-    else:
-        print("You lost!")
+def hangman_art(mistakes):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(mistakes == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(mistakes == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(mistakes == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(mistakes == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(mistakes == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(mistakes == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (mistakes == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+
+# run the file, since it is in a "bigger one" AKA "all_games.py"
+if(__name__ == "__main__"): 
+    play_hangman()
