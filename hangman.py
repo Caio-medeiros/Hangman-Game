686eb7727fa843f081b7518a98cb8dd983089d46
Caio Medeiros <63916640+Caio-medeiros@users.noreply.github.com>
2021-08-05 20:01:46 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,22 +1,34 @@
+import random
+
     print("\n****************************")
     print("Welcome to the hangman game!")
     print("****************************")
 
-    secret_word = "Apple".upper() #you can choose the word here!
-   
+    file = open("words.txt", "r")
+    words = []
+
+    for line in file:
+        line = line.strip()
+        words.append(line)
+
+    file.close()
+
+    number = random.randrange(0, len(words))
+    secret_word = words[number].upper()
+    # para cada letra na palavra adicione no final da lista "_"
     guessed_letters = ["_" for letter in secret_word]
 
     hanged = False
-    got_it_right = False
+    got_it_right = False  # O jogo ainda não começou por isso deixamos "False"
     mistakes = 0
 
-    while(not hanged and not got_it_right):
+    while(not hanged and not got_it_right):  # Enquanto(True and True) = Enquanto(True)
         guess = input("\nWhich Letter? ")
         guess = guess.strip().upper()
 
         if (guess in secret_word):
             index = 0
-          
+            # você não precisa declara explicitamente o tipo da variável que será utilizada no for como índice da execução.
             for letter in secret_word:
                 if(guess == letter):
                     guessed_letters[index] = letter
